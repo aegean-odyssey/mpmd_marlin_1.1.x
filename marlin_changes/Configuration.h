4a88b93745cdf4618a035e7ed06c7aae5ca42e71
Aegean Odyssey <57586704+aegean-odyssey@users.noreply.github.com>
2020-10-06 18:21:50 +0000

    tweak slow tap speed

--- marlin_changes/Configuration.h
+++ marlin_changes/Configuration.h
@@ -1043,7 +1043,7 @@
 #define Z_PROBE_SPEED_FAST  (HOMING_FEEDRATE_Z / 5)
 
 // Feedrate (mm/m) for the "accurate" probe of each point
-#define Z_PROBE_SPEED_SLOW  (Z_PROBE_SPEED_FAST / 2)
+#define Z_PROBE_SPEED_SLOW  (Z_PROBE_SPEED_FAST / 3)
 
 // The number of probes to perform at each point.
 //   Set to 2 for a fast/slow probe, using the second probe result.
