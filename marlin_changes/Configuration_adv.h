b3b9b14daf2b313b5585f01d495b11759b769e62
Aegean Odyssey <57586704+aegean-odyssey@users.noreply.github.com>
2020-12-31 00:55:29 +0000

    Big clean up. Rework temperature.cpp for clarity and reduced code size. Enable SHOW_TEMP_ADC_VALUES to prepare for some thermistor table improvements.

--- marlin_changes/Configuration_adv.h
+++ marlin_changes/Configuration_adv.h
@@ -146,7 +146,7 @@
 
 // Show Temperature ADC value
 // Enable for M105 to include ADC values read from temperature sensors.
-//#define SHOW_TEMP_ADC_VALUES
+#define SHOW_TEMP_ADC_VALUES
 
 /**
  * High Temperature Thermistor Support
